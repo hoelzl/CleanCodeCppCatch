375ce2d8877986e82a515eede6850839b5b538eb
Matthias Hölzl <tc@xantira.com>
2020-12-01 04:44:47 +0100

    Add Ox example

---
+++ Ox/core.cpp
@@ -0,0 +1,93 @@
+//
+// Created by tc on 08/07/2020.
+//
+#include "core.h"
+
+CharacterMatch::CharacterMatch(char proposed_char) : proposed_char(proposed_char)
+{
+}
+
+PerfectMatch::PerfectMatch(char proposed_char) : CharacterMatch(proposed_char)
+{
+}
+
+bool PerfectMatch::is_perfect_match() const
+{
+    return true;
+}
+
+bool PerfectMatch::is_partial_match() const
+{
+    return true;
+}
+
+PartialMatch::PartialMatch(char proposed_char) : CharacterMatch(proposed_char)
+{
+}
+
+bool PartialMatch::is_perfect_match() const
+{
+    return false;
+}
+
+bool PartialMatch::is_partial_match() const
+{
+    return true;
+}
+
+FailedMatch::FailedMatch(char proposed_char) : CharacterMatch(proposed_char)
+{
+}
+
+bool FailedMatch::is_perfect_match() const
+{
+    return false;
+}
+
+bool FailedMatch::is_partial_match() const
+{
+    return false;
+}
+
+std::unique_ptr<CharacterMatch> get_character_match(char guess, std::size_t index, const std::string& solution)
+{
+    if (index < solution.size() && solution[index] == guess) {
+        return std::make_unique<PerfectMatch>(guess);
+    }
+    if (solution.find(guess) != std::string::npos) {
+        return std::make_unique<PartialMatch>(guess);
+    }
+    return std::make_unique<FailedMatch>(guess);
+}
+
+Match::Match(const std::string& word_to_guess, const std::string& proposed_solution)
+    : word_to_guess(word_to_guess), proposed_solution(proposed_solution)
+{
+    for (int i{0}; i < word_to_guess.size(); ++i) {
+        character_matches.push_back(get_character_match(word_to_guess[i], i, proposed_solution));
+    }
+}
+bool Match::is_perfect_match() const
+{
+    for (auto& char_match : character_matches) {
+        if (!char_match->is_perfect_match()) {
+            return false;
+        }
+    }
+    return true;
+}
+
+std::string Match::describe() const
+{
+    std::string result{};
+    for (auto& char_match : character_matches) {
+        if (char_match->is_perfect_match()) {
+            result += "+";
+        } else if (char_match->is_partial_match()) {
+            result += "-";
+        } else {
+            result += ".";
+        }
+    }
+    return result;
+}
---
+++ Ox/core.h
@@ -0,0 +1,61 @@
+#ifndef OX_CORE_H
+#define OX_CORE_H
+
+#include <memory>
+#include <string>
+#include <vector>
+
+class CharacterMatch
+{
+protected:
+    char proposed_char{};
+
+public:
+    explicit CharacterMatch(char proposed_char);
+    [[nodiscard]] virtual bool is_perfect_match() const = 0;
+    [[nodiscard]] virtual bool is_partial_match() const = 0;
+};
+
+class PerfectMatch : public CharacterMatch
+{
+public:
+    explicit PerfectMatch(char proposed_char);
+    [[nodiscard]] bool is_perfect_match() const override;
+    [[nodiscard]] bool is_partial_match() const override;
+};
+
+
+class PartialMatch : public CharacterMatch
+{
+public:
+    explicit PartialMatch(char proposed_char);
+    [[nodiscard]] bool is_perfect_match() const override;
+    [[nodiscard]] bool is_partial_match() const override;
+};
+
+
+class FailedMatch : public CharacterMatch
+{
+public:
+    explicit FailedMatch(char proposed_char);
+    [[nodiscard]] bool is_perfect_match() const override;
+    [[nodiscard]] bool is_partial_match() const override;
+};
+
+std::unique_ptr<CharacterMatch> get_character_match(char guess, std::size_t index, const std::string& solution);
+
+class Match
+{
+public:
+    Match(const std::string& word_to_guess, const std::string& proposed_solution);
+    [[nodiscard]] bool is_perfect_match() const;
+    [[nodiscard]] std::string describe() const;
+
+private:
+    std::string word_to_guess{};
+    std::string proposed_solution{};
+    std::vector<std::unique_ptr<CharacterMatch>> character_matches{};
+};
+
+
+#endif//OX_CORE_H
